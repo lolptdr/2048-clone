69afa8ff9fdc7ece3ba3ba5ede5e0125158f303f
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-15 23:52:41 -0500

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name 2048App
+ * @description
+ * # 2048App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('2048App', [
+    'ngCookies'
+  ]);
